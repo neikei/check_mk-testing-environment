2025572f90372c3337232bbd466eb197b89b74a6
neikei <niklas@vosskoetter.org>
2019-12-03 20:12:04 +0100

    Updated Ansible from 2.6 to 2.9

--- ansible/tools/install_ansible_in_Vagrantbox.sh
+++ ansible/tools/install_ansible_in_Vagrantbox.sh
@@ -9,9 +9,14 @@ if [ "$ansible_installed" -eq 0 ]; then
     sudo apt install -y dirmngr
   fi
 
-  # Add ansible 2.6 repository and install ansible
+  # Install gnupg if Debian major version is 10 (required for apt-key and apt)
+  if [[ "$debian_version" =~ ^10.*$ ]]; then
+    sudo apt install -y gnupg python-apt
+  fi
+
+  # Add ansible 2.9 repository and install ansible
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  echo "deb http://ppa.launchpad.net/ansible/ansible-2.6/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/ansible.list
+  echo "deb http://ppa.launchpad.net/ansible/ansible-2.9/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/ansible.list
   sudo apt update -y
   sudo apt install -y ansible
 
